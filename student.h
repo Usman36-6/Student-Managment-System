d0b5f80d04f5121532606b7ae08e9158eef61e9f
Usman <giraffstar974@gmail.com>
2025-06-13 11:03:54 +0500

    Update student.h

--- student.h
+++ student.h
@@ -1,24 +1,5 @@
 
-/*#ifndef STUDENT_H
-#define STUDENT_H
 
-typedef struct
-{
-    int id;
-    char name[50];
-    float marks;
-} Student;
-
-void addStudent();
-void displayStudents();
-void searchStudent(int id);
-void deleteStudent(int id);
-void updateStudent(int id);
-char getGrade(float marks);
-
-#endif*/
-
-// student.h
 #ifndef STUDENT_H
 #define STUDENT_H
 
